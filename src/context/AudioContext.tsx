47b968ce918d54f0d2dd7b475f7a0e80d84ac1a2
nitives <cadenrichards15@gmail.com>
2024-08-31 00:44:28 -0400

    Fixed current song playing again when clicking next

--- src/context/AudioContext.tsx
+++ src/context/AudioContext.tsx
@@ -43,29 +43,22 @@ export const AudioProvider: React.FC<{ children: React.ReactNode }> = ({
   const playNextTrack = async () => {
     console.log("Playing next track...");
     console.log("Current track:", currentTrack);
+    console.log("Next track:", playlist[1]);
     console.log("Playlist:", playlist);
-    console.log("Playlist URL:", playlistUrl);
-    if (currentTrack && playlist.length > 0) {
-      const currentIndex = playlist.findIndex(
-        (track) => track.id === currentTrack.id
-      );
-      console.log("Current index:", currentIndex);
-      if (currentIndex < playlist.length - 1) {
-        const nextTrack = playlist[currentIndex + 1];
-        console.log("Next track:", nextTrack);
-        setCurrentTrack(nextTrack);
-        const response = await fetch(`/api/track/info/${nextTrack.id}`);
-        const nextTrackData = await response.json();
-        console.log("Next track data:", nextTrackData);
-        setPlaylistUrl(await fetchPlaylistM3U8(nextTrackData.permalink_url));
+    if (playlist.length > 1) {
+      const nextTrack = playlist[1];
+      setCurrentTrack(nextTrack);
+      const response = await fetch(`/api/track/info/${nextTrack.id}`);
+      const nextTrackData = await response.json();
+      setPlaylistUrl(await fetchPlaylistM3U8(nextTrackData.permalink_url));
 
-        // Fetch HD cover
-        await fetchCover(nextTrackData.permalink_url);
-      } else {
-        console.log("Reached end of playlist");
-      }
+      // Fetch HD cover
+      await fetchCover(nextTrackData.permalink_url);
+
+      // Remove the first song from the playlist
+      setPlaylist((prevPlaylist) => prevPlaylist.slice(1));
     } else {
-      console.log("No current track or empty playlist");
+      console.log("No more tracks in the playlist");
     }
     console.log("----------------------------");
   };
