e7500a8456fd351b9982b2840ff7938deae8cf76
nitives <cadenrichards15@gmail.com>
2025-03-05 21:54:32 -0500

    SeeAllPage | Fixing Error Deployment

--- src/app/(pages)/artist/[artist_name]/[artist_id]/see-all/page.tsx
+++ src/app/(pages)/artist/[artist_name]/[artist_id]/see-all/page.tsx
@@ -2,17 +2,7 @@
 import { dev } from "@/lib/utils";
 import { useSearchParams } from "next/navigation";
 
-interface SeeAllPageProps {
-  params: {
-    artist_name: string;
-    artist_id: string;
-  };
-}
-
-export default function SeeAllPage({ params }: SeeAllPageProps) {
-  const searchParams = useSearchParams();
-  dev.log("params", params);
-
+export default function SeeAllPage() {
   return (
     <div>
       <h2>{/* Artist: {params.artist_name} (ID: {params.artist_id}) */}</h2>
