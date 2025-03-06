31fa3b9a15fe94d8b62cd2ba92545a20e0316bf5
nitives <cadenrichards15@gmail.com>
2025-03-06 02:52:01 -0500

    PlaylistPage | Fixing Error Deployment

--- src/app/(pages)/artist/[artist_name]/[artist_id]/see-all/page.tsx
+++ src/app/(pages)/artist/[artist_name]/[artist_id]/see-all/page.tsx
@@ -1,14 +1,7 @@
-"use client";
-import { dev } from "@/lib/utils";
-import { useSearchParams } from "next/navigation";
-
 export default function SeeAllPage() {
   return (
     <div>
-      <h2>{/* Artist: {params.artist_name} (ID: {params.artist_id}) */}</h2>
       <p>See All Page isn&apos;t finished yet</p>
-      {/* <p>Section requested: {section}</p> */}
-      {/* ...render "full-albums", "singles", "top-songs", etc. based on section... */}
     </div>
   );
 }
--- src/app/(pages)/playlist/[playlist_id]/page.tsx
+++ src/app/(pages)/playlist/[playlist_id]/page.tsx
@@ -1,15 +1,7 @@
-interface PlaylistPageProps {
-  params: {
-    playlist_id: string;
-  };
-}
-
-export default function PlaylistPage({ params }: PlaylistPageProps) {
+export default function PlaylistPage() {
   return (
     <div>
-      <h2>Playlist ID: {params.playlist_id}</h2>
       <p>Playlist page isn&apos;t finished yet</p>
-      {/* Your playlist detail content goes here */}
     </div>
   );
 }
